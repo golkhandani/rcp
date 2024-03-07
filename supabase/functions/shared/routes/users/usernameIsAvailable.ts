d5c4a13dd4d308dd04179e369051e7477f0b509a
golkhandani <golkhandani@gmail.com>
2024-03-06 17:55:25 -0800

    cleanup(be): complete refactor edge functions

--- supabase/functions/shared/routes/users/usernameIsAvailable.ts
+++ supabase/functions/shared/routes/users/usernameIsAvailable.ts
@@ -9,25 +9,36 @@ export async function usernameIsAvailable(
 	// TODO(@golkhandani): parse and validate input
 	const query = req.query;
 	// Get supabase client and required user data
-	const { admin, supabase, user, authHeader } = await getClientInfo(req);
+	const { admin, user } = await getClientInfo(req);
 
-	const { data: userProfile, error } = await admin.from(userProfileDb)
+	const { data: userProfiles, error } = await admin.from(userProfileDb)
 		.select()
 		.eq(
 			'username',
 			query.username,
 		).neq('user_id', user.id)
-		.single<UserProfileRow>();
+		.limit(1)
+		.returns<UserProfileRow[]>();
 
-	if (error) {
-		console.error(error);
-		throw new ApiError('Cannot get data! Please try again!', 500, 20500);
-	}
 	const data = {
 		isAvailable: false,
 	};
 
-	if (!userProfile) {
+	if (error) {
+		if (error.code == 'PGRST116' && !userProfiles || userProfiles) {
+			// "The result contains 0 rows" => safe error
+
+			data.isAvailable = true;
+		} else {
+			console.error(error);
+			throw new ApiError(
+				'Cannot get data! Please try again!',
+				500,
+				20500,
+			);
+		}
+	}
+	if (userProfiles?.length == 0) {
 		data.isAvailable = true;
 	}
 
